4768528ed48644246c8563b89228b158e0088012
Slava Andrejev <vyac.andrejev@gmail.com>
2021-11-15 19:17:51 -0800

    Textures exercise 1

---
+++ 01. Getting started/04. Textures/03. Exercises/001/render.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include <memory>
+
+#include <glibmm/refptr.h>
+#include <gtkmm/builder.h>
+#include <gtkmm/glarea.h>
+
+#include <boost/hana/functional/fix.hpp>
+
+#include <epoxy/gl.h>
+#include <epoxy/glx.h>
+
+#include <shader.h>
+
+class OpenGLRender : public Gtk::GLArea
+{
+public:
+    OpenGLRender(BaseObjectType* cobject,
+                 const Glib::RefPtr<Gtk::Builder>& refBuilder);
+private:
+    bool on_render(const Glib::RefPtr<Gdk::GLContext>& context) override;
+    void on_realize() override;
+    void on_unrealize() override;
+
+    std::unique_ptr<Shader> renderingProgram;
+
+    GLuint VBO = {};
+    GLuint VAO = {};
+    GLuint EBO = {};
+    GLuint texture[2] = {};
+};
