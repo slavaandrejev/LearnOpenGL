56f872a5d3b46c151e0f4b5d4dbdb3074071e10a
Slava Andrejev <vyac.andrejev@gmail.com>
2022-12-08 14:02:39 -0800

    Remove unused code

--- 01. Getting started/02. Hello Triangle/03. Exercises/003/render.h
+++ 01. Getting started/02. Hello Triangle/03. Exercises/003/render.h
@@ -27,42 +27,6 @@ private:
     void on_realize() override;
     void on_unrealize() override;
 
-    template <typename ...Args>
-    GLuint CompileShaders(Args ...args) {
-        namespace hana = boost::hana;
-        auto program = glCreateProgram();
-
-        auto compileFromResource = [](auto path, auto shaderType) -> GLuint {
-            auto bytes = Gio::Resource::lookup_data_global(path);
-            auto size = gsize{};
-            auto data = reinterpret_cast<const char*>(bytes->get_data(size));
-
-            auto shader = glCreateShader(shaderType);
-            glShaderSource(shader, 1, &data, nullptr);
-            glCompileShader(shader);
-
-            return shader;
-        };
-
-        auto shaders = std::vector<GLuint>{};
-        auto attachShaders = hana::fix([&](auto attshdr, auto path, auto type, auto ...args) {
-            auto shader = compileFromResource(path, type);
-            shaders.push_back(shader);
-            glAttachShader(program, shader);
-            if constexpr (0 < sizeof...(args)) {
-                attshdr(args...);
-            }
-        });
-
-        attachShaders(args...);
-        glLinkProgram(program);
-        for (auto shader : shaders) {
-            glDeleteShader(shader);
-        }
-
-        return program;
-    }
-
     std::array<std::unique_ptr<Shader>, 2> renderingProgram;
 
     GLuint VBO[2] = {};
