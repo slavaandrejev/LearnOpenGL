72503b8ee72681106c62012b83acc365c71e38ee
Slava Andrejev <vyac.andrejev@gmail.com>
2022-12-09 15:40:41 -0800

    Transformations exercise 2

---
+++ 01. Getting started/05. Transformations/02. Exercises/002/render.cpp
@@ -0,0 +1,144 @@
+#include <gdkmm/frameclock.h>
+#include <gdkmm/pixbuf.h>
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+#include "render.h"
+
+OpenGLRender::OpenGLRender(BaseObjectType* cobject,
+                           const Glib::RefPtr<Gtk::Builder>& refBuilder)
+  : Gtk::GLArea(cobject)
+{}
+
+bool OpenGLRender::on_render(const Glib::RefPtr<Gdk::GLContext>& context) {
+    const GLfloat color[] = {0.2f, 0.3f, 0.3f, 1.0f};
+    glClearBufferfv(GL_COLOR, 0, color);
+
+    glBindTextureUnit(0, texture[0]);
+    glBindTextureUnit(1, texture[1]);
+
+    renderingProgram->use();
+    auto trans = glm::translate(glm::mat4{1.0f}, glm::vec3(0.5f, -0.5f, 0.0f));
+    trans = glm::rotate(trans, curTime, glm::vec3(0.0f, 0.0f, 1.0f));
+    renderingProgram->set("transform", trans);
+
+    glBindVertexArray(VAO);
+    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
+
+    trans = glm::translate(glm::mat4{1.0f}, glm::vec3(-0.5f, 0.5f, 0.0f));
+    trans = glm::scale(trans, glm::vec3{std::sin(curTime)});
+    renderingProgram->set("transform", trans);
+    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
+
+    return true;
+}
+
+void OpenGLRender::on_realize() {
+    Gtk::GLArea::on_realize();
+    make_current();
+
+    glCreateTextures(GL_TEXTURE_2D, 2, &texture[0]);
+
+    auto contImg = Gdk::Pixbuf::create_from_resource("/container.jpg");
+    glTextureParameteri(texture[0], GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTextureParameteri(texture[0], GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTextureParameteri(texture[0], GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+    glTextureParameteri(texture[0], GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTextureStorage2D(texture[0], 1, GL_RGB8, contImg->get_width(), contImg->get_height());
+    glTextureSubImage2D(
+        texture[0]
+      , 0
+      , 0
+      , 0
+      , contImg->get_width()
+      , contImg->get_height()
+      , GL_RGB
+      , GL_UNSIGNED_BYTE
+      , contImg->get_pixels());
+    glGenerateTextureMipmap(texture[0]);
+    contImg.reset();
+
+    auto faceImg = Gdk::Pixbuf::create_from_resource("/awesomeface.png");
+    glTextureParameteri(texture[1], GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTextureParameteri(texture[1], GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTextureParameteri(texture[1], GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+    glTextureParameteri(texture[1], GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTextureStorage2D(texture[1], 1, GL_RGBA8, faceImg->get_width(), faceImg->get_height());
+    glTextureSubImage2D(
+        texture[1]
+      , 0
+      , 0
+      , 0
+      , faceImg->get_width()
+      , faceImg->get_height()
+      , GL_RGBA
+      , GL_UNSIGNED_BYTE
+      , faceImg->get_pixels());
+    glGenerateTextureMipmap(texture[1]);
+    faceImg.reset();
+
+    renderingProgram = std::make_unique<Shader>(
+        "/vs.glsl", GL_VERTEX_SHADER,
+        "/fs.glsl", GL_FRAGMENT_SHADER);
+
+    renderingProgram->use();
+    renderingProgram->set("texture1", 0);
+    renderingProgram->set("texture2", 1);
+
+    float vertices[] = {
+        // positions       // texture coords
+        0.5f,  0.5f, 0.0f, 1.0f, 1.0f, // top right
+        0.5f, -0.5f, 0.0f, 1.0f, 0.0f, // bottom right
+       -0.5f, -0.5f, 0.0f, 0.0f, 0.0f, // bottom left
+       -0.5f,  0.5f, 0.0f, 0.0f, 1.0f  // top left
+    };
+    GLuint indices[] = {
+        0, 1, 3, // first triangle
+        1, 2, 3  // second triangle
+    };
+
+    glCreateVertexArrays(1, &VAO);
+    glBindVertexArray(VAO);
+    glCreateBuffers(1, &VBO);
+    glCreateBuffers(1, &EBO);
+
+    glNamedBufferStorage(VBO, sizeof(vertices), &vertices[0], 0);
+    glNamedBufferStorage(EBO, sizeof(indices), &indices[0], 0);
+
+    glVertexArrayAttribBinding(VAO, 0, 0);
+    glVertexArrayAttribFormat(VAO, 0, 3, GL_FLOAT, GL_FALSE, 0);
+    glEnableVertexArrayAttrib(VAO, 0);
+
+    glVertexArrayAttribBinding(VAO, 1, 0);
+    glVertexArrayAttribFormat(VAO, 1, 2, GL_FLOAT, GL_FALSE, 3 * sizeof(float));
+    glEnableVertexArrayAttrib(VAO, 1);
+
+    glVertexArrayVertexBuffer(VAO, 0, VBO, 0, 5 * sizeof(float));
+    glVertexArrayElementBuffer(VAO, EBO);
+
+    add_tick_callback(sigc::mem_fun(*this, &OpenGLRender::timer_event));
+}
+
+void OpenGLRender::on_unrealize() {
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+    glDeleteBuffers(1, &EBO);
+    glDeleteTextures(2, &texture[0]);
+    renderingProgram.reset();
+
+    Gtk::GLArea::on_unrealize();
+}
+
+bool OpenGLRender::timer_event(const Glib::RefPtr<Gdk::FrameClock>& frameClock) {
+    if (0 > startTime) {
+        startTime = frameClock->get_frame_time();
+        curTime   = 0;
+    } else {
+        curTime = 1e-6f * (frameClock->get_frame_time() - startTime);
+    }
+    queue_draw();
+
+    return true;
+}
