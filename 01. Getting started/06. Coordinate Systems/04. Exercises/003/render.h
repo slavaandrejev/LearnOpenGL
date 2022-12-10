48cf429c1948df68ae3b9a0155182e52aed622c8
Slava Andrejev <vyac.andrejev@gmail.com>
2022-12-10 15:09:33 -0800

    Coordinate Systems exercise 3

---
+++ 01. Getting started/06. Coordinate Systems/04. Exercises/003/render.h
@@ -0,0 +1,39 @@
+#pragma once
+
+#include <memory>
+#include <vector>
+
+#include <gdkmm/frameclock.h>
+#include <glibmm/refptr.h>
+#include <gtkmm/builder.h>
+#include <gtkmm/glarea.h>
+
+#include <epoxy/gl.h>
+
+#include <glm/glm.hpp>
+
+#include <shader.h>
+
+class OpenGLRender : public Gtk::GLArea
+{
+public:
+    OpenGLRender(BaseObjectType* cobject,
+                 const Glib::RefPtr<Gtk::Builder>& refBuilder);
+private:
+    bool on_render(const Glib::RefPtr<Gdk::GLContext>& context) override;
+    void on_realize() override;
+    void on_unrealize() override;
+
+    bool timer_event(const Glib::RefPtr<Gdk::FrameClock>&);
+
+    std::unique_ptr<Shader> renderingProgram;
+
+    GLuint VBO = {};
+    GLuint VAO = {};
+    GLuint texture[2] = {};
+
+    std::vector<glm::vec3> cubePositions;
+
+    gint64 startTime = -1;
+    float  curTime   = -1.0f;
+};
