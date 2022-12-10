ca2bf3ef2f61ef19fe67ffb98531cd0ecab781a7
Slava Andrejev <vyac.andrejev@gmail.com>
2022-12-10 13:09:07 -0800

    Coordinate Systems, cube with depth testing

---
+++ 01. Getting started/06. Coordinate Systems/02. Cube/render.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <memory>
+
+#include <gdkmm/frameclock.h>
+#include <glibmm/refptr.h>
+#include <gtkmm/builder.h>
+#include <gtkmm/glarea.h>
+
+#include <epoxy/gl.h>
+
+#include <shader.h>
+
+class OpenGLRender : public Gtk::GLArea
+{
+public:
+    OpenGLRender(BaseObjectType* cobject,
+                 const Glib::RefPtr<Gtk::Builder>& refBuilder);
+private:
+    bool on_render(const Glib::RefPtr<Gdk::GLContext>& context) override;
+    void on_realize() override;
+    void on_unrealize() override;
+
+    bool timer_event(const Glib::RefPtr<Gdk::FrameClock>&);
+
+    std::unique_ptr<Shader> renderingProgram;
+
+    GLuint VBO = {};
+    GLuint VAO = {};
+    GLuint texture[2] = {};
+
+    gint64 startTime = -1;
+    float  curTime   = -1.0f;
+};
