2f5491408dafa765d075f72d1e8898d59151999a
Slava Andrejev <vyac.andrejev@gmail.com>
2022-12-25 21:07:57 -0800

    Materials/Change light cube color exercise

---
+++ 02. Lighting/03. Materials/02. Exercises/001/ex-001-window.cpp
@@ -0,0 +1,20 @@
+#include <glibmm/refptr.h>
+
+#include "ex-001-window.h"
+
+MainWindow::MainWindow(BaseObjectType* cobject,
+                       const Glib::RefPtr<Gtk::Builder>& refBuilder)
+  : Gtk::ApplicationWindow(cobject)
+  , refBuilder(refBuilder)
+{
+    glArea = Gtk::Builder::get_widget_derived<OpenGLRender>(refBuilder, "glArea");
+}
+
+MainWindow* MainWindow::create() {
+    auto refBuilder = Gtk::Builder::create_from_resource("/ex-001-window.ui");
+    auto window     = Gtk::Builder::get_widget_derived<MainWindow>(refBuilder, "mainWindow");
+    if (!window) {
+        throw std::runtime_error(R"(No "mainWindow" object in ex-001-window.ui)");
+    }
+    return window;
+}
---
+++ 02. Lighting/03. Materials/02. Exercises/001/render.h
@@ -0,0 +1,60 @@
+#pragma once
+
+#include <memory>
+#include <vector>
+
+#include <gdkmm/frameclock.h>
+#include <glibmm/refptr.h>
+#include <gtkmm/builder.h>
+#include <gtkmm/eventcontrollerkey.h>
+#include <gtkmm/eventcontrollermotion.h>
+#include <gtkmm/eventcontrollerscroll.h>
+#include <gtkmm/gestureclick.h>
+#include <gtkmm/glarea.h>
+
+#include <epoxy/gl.h>
+
+#include <glm/glm.hpp>
+
+#include <camera.h>
+#include <shader.h>
+
+class OpenGLRender : public Gtk::GLArea
+{
+public:
+    OpenGLRender(BaseObjectType* cobject,
+                 const Glib::RefPtr<Gtk::Builder>& refBuilder);
+private:
+    bool on_render(const Glib::RefPtr<Gdk::GLContext>& context) override;
+    void on_realize() override;
+    void on_unrealize() override;
+
+    bool on_key_pressed(guint keyval, guint keycode, Gdk::ModifierType state);
+    void on_key_released(guint keyval, guint keycode, Gdk::ModifierType state);
+    Glib::RefPtr<Gtk::EventControllerKey> keyEvents;
+
+    Glib::RefPtr<Gtk::EventControllerMotion> mouseMoveEvents;
+    Glib::RefPtr<Gtk::EventControllerScroll> scrollEvents;
+    Glib::RefPtr<Gtk::GestureClick> clickEvents;
+
+    bool timer_event(const Glib::RefPtr<Gdk::FrameClock>&);
+
+    std::unique_ptr<Shader> lightingShader;
+    std::unique_ptr<Shader> lightCubeShader;
+
+    GLuint VBO = {};
+    GLuint cubeVAO = {};
+    GLuint lightCubeVAO = {};
+
+    glm::vec3 lightPos{1.2f, 1.0f, 2.0f};
+
+    bool mouseGrabbed = false;
+    Camera camera{
+        {-1.68f, -1.22f, 2.39f}
+      , {0.0f, 1.0f, 0.0f}
+      , {0.68f, 0.48f, -0.56f}
+      };
+
+    gint64 startTime = -1;
+    float  curTime   = -1.0f;
+};
