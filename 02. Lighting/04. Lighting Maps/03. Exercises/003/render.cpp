b3e158000213e0c235e7bd108b460e17280b6f39
Slava Andrejev <vyac.andrejev@gmail.com>
2023-02-25 15:02:00 -0800

    Remove copy&paste of LoadTextureFromResources

--- 02. Lighting/04. Lighting Maps/03. Exercises/003/render.cpp
+++ 02. Lighting/04. Lighting Maps/03. Exercises/003/render.cpp
@@ -1,6 +1,5 @@
 #include <gdk/gdkkeysyms.h>
 #include <gdkmm/frameclock.h>
-#include <gdkmm/pixbuf.h>
 #include <giomm/resource.h>
 #include <glibmm/refptr.h>
 #include <gtkmm/builder.h>
@@ -16,9 +15,9 @@
 
 #include <epoxy/gl.h>
 
-#include "render.h"
+#include <load_texture.h>
 
-GLuint LoadTextureFromResources(const std::string &path);
+#include "render.h"
 
 OpenGLRender::OpenGLRender(BaseObjectType* cobject,
                            const Glib::RefPtr<Gtk::Builder>& refBuilder)
@@ -270,48 +269,4 @@ bool OpenGLRender::timer_event(const Glib::RefPtr<Gdk::FrameClock>& frameClock) 
     }
 
     return true;
-}
-
-GLuint LoadTextureFromResources(const std::string &path) {
-    auto img = Gdk::Pixbuf::create_from_resource(path);
-    auto tex = GLuint{};
-
-    glCreateTextures(GL_TEXTURE_2D, 1, &tex);
-    auto format         = GLenum{};
-    auto internalFormat = GLenum{};
-    switch (img->get_n_channels()) {
-        case 1: {
-            format =  GL_RED;
-            internalFormat = GL_R8;
-            break;
-        }
-        case 3: {
-            format =  GL_RGB;
-            internalFormat = GL_RGB8;
-            break;
-        }
-        case 4: {
-            format =  GL_RGBA;
-            internalFormat = GL_RGBA8;
-            break;
-        }
-    }
-    glTextureParameteri(tex, GL_TEXTURE_WRAP_S, GL_REPEAT);
-    glTextureParameteri(tex, GL_TEXTURE_WRAP_T, GL_REPEAT);
-    glTextureParameteri(tex, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
-    glTextureParameteri(tex, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-    glTextureStorage2D(tex, 1, internalFormat, img->get_width(), img->get_height());
-    glTextureSubImage2D(
-        tex
-      , 0
-      , 0
-      , 0
-      , img->get_width()
-      , img->get_height()
-      , format
-      , GL_UNSIGNED_BYTE
-      , img->get_pixels());
-    glGenerateTextureMipmap(tex);
-
-    return tex;
 }
